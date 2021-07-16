4c685dc28dc7e4dd578d036ef90719ef47819ba8
Yoale Robledo <55332765+yoaroib@users.noreply.github.com>
2021-07-15 19:57:15 -0500

    Webdriver change
    Changed webdriver to Chrome and added basic options to minimize interaction with computer.

--- citas_sat.py
+++ citas_sat.py
@@ -1,11 +1,23 @@
 from time import sleep
-from selenium.webdriver import Firefox
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 
-driver = Firefox()
+driver = Chrome()
+
+wait_time = 180
+# Uncomment the next variable and enter your RFC
+#rfc = ''
+mail = 'your@mail.com'
+
 driver.get('https://citas.sat.gob.mx/citasat/agregarcita.aspx')
 
 input('Press Enter to start....')
+
+# Uncomment next line to automatically fill the RFC
+#driver.find_element_by_id('TXTRFC').send_keys(rfc + Keys.TAB)
+#sleep(2)
+driver.find_element_by_id('TXTCorreoElectronico').send_keys(mail + Keys.TAB)
+sleep(2)
 
 def searchDates():
     try:
@@ -14,18 +26,20 @@ def searchDates():
         for fecha in fechas:
             verificacion = fecha.get_attribute('style')
 
-            if ('rgb(74, 144, 226)' in verificacion or 'rgb(250, 230, 226)' in verificacion):
-                driver.execute_script('''window.open("https://youtu.be/gcNEC9NaJuE?t=10","_blank");''')
+            if ('rgb(74, 144, 226)' in verificacion or 'rgb(250, 235, 204)' in verificacion):
+                driver.execute_script('''window.open("https://www.youtube.com/watch?v=qe5-ywmuKOg","_blank");''')
                 terminar = input('End program (default yes)? [y/n]')
 
                 if terminar == 'y':
+                    driver.quit()
                     return True
                 elif terminar == 'n':
                     searchDates()
                 else:
+                    driver.quit()
                     return True
 
-        sleep(180)
+        sleep(wait_time)
         driver.refresh()
         alert = driver.switch_to.alert
         alert.accept()
